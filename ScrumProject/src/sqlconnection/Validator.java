72c2eb97e315f0a52a0ceb1eff65107444f44854
Tomas <Tomas@DESKTOP-10KSTEL>
2017-01-26 14:31:00 +0100

    Inloggning/MatildaM

---
+++ ScrumProject/src/sqlconnection/Validator.java
@@ -0,0 +1,69 @@
+/*
+ * This is the validation class. In this class we validate our code that communicates with the database.
+ */
+package sqlconnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import sqlconnection.SQLConnection;
+
+/**
+ *
+ * @author Tomas
+ */
+public class Validator {
+    
+    public static boolean LoginIsValid(String LoginString){
+        boolean exists = false;
+                ArrayList<String> loginList = new ArrayList<String>();
+        try {
+            ResultSet userNameRes = SQLConnection.getDatabas().executeQuery("select AnvandarNamn from Anvandare");
+            ResultSet passwordRes = SQLConnection.getDatabas().executeQuery("select Losenord from Anvandare");
+            while(userNameRes.next()&& passwordRes.next()){
+                String ThisUserName = userNameRes.getString(1);
+                String ThisPassword = passwordRes.getString(1);
+                String Login = ThisUserName+ThisPassword;
+                loginList.add(Login);           
+            }
+            
+            for(String log:loginList){
+                if(log.equals(LoginString)){
+                    exists = true;
+                }
+            }         
+        }
+      
+         catch (Exception ex) {
+            System.out.println("fel i validator.LoginIsValid :"+ex);
+        }
+        return exists;
+    }
+    
+    
+    public static boolean PasswordExists(String password){
+        Boolean exists = false;
+        ArrayList<String> passwords = new ArrayList<String>();
+        
+        try{
+       ResultSet passwordList = SQLConnection.getDatabas().executeQuery("select Losenord from anvandare");
+                   while(passwordList.next()){
+        String existingPasswords = passwordList.getString(1);
+        passwords.add(existingPasswords);
+            }
+                   for(String pass:passwords){
+                       if(pass == password){
+                           exists = true;
+                       }
+                       else{
+                           exists = false;
+                       }
+                   }        
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+    }
+    return exists;
+}
+}
